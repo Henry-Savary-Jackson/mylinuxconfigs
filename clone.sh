910b15c092a1a8d24c2b24d8f88c778e0a16d6fa
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2023-12-01 04:24:37 +0200

    initial commit

---
+++ clone.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+sudo pacman -Sy github-cli
+git clone https://github.com/Henry-Savary-Jackson/mydotfiles ~/
+
+echo installing my dotfiles
+cd ~/mydotfiles
+
+./install.sh
+
+echo logging into github account
+gh auth login
+
+echo cloning mypacmanpackages
+git clone https://github.com/Henry-Savary-Jackson/mypacmanpackages ~/
+
+echo cloning mydwmbuild 
+git clone https://github.com/Henry-Savary-Jackson/mydwmbuild ~/
+
+echo cloning mystbuild 
+git clone https://github.com/Henry-Savary-Jackson/mystbuild ~/
+
+echo cloning mydwmblocksbuild 
+git clone https://github.com/Henry-Savary-Jackson/mydwmblocksbuild ~/
+
+echo cloning mydmenubuild 
+git clone https://github.com/Henry-Savary-Jackson/mydmenubuild ~/
+
+echo cloning myrangerbuild 
+git clone https://github.com/Henry-Savary-Jackson/myrangerbuild ~/
+
+echo cloning mynvimconfig 
+git clone https://github.com/Henry-Savary-Jackson/mynvimconfig ~/
+
+echo cloning password-store 
+git clone https://github.com/Henry-Savary-Jackson/mynvimhttpconfig ~/.password-store
+
+echo cloning mozillaconfig 
+git clone https://github.com/Henry-Savary-Jackson/mozillaconfig ~/
+
+echo cloning mycodingstuff 
+git clone https://github.com/Henry-Savary-Jackson/mycodingstuff ~/
+
+echo done

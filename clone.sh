4d35c95cef7bd97db89423b3e441105413176370
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2023-12-16 00:23:13 +0200

    added more automation to script

--- clone.sh
+++ clone.sh
@@ -2,46 +2,74 @@
 
 inv_user=$USER
 echo $inv_user
+
+ln -f $(pwd)/.gitconfig ~/
+
+
+
 sudo pacman -Sy github-cli
-git clone https://github.com/Henry-Savary-Jackson/mydotfiles /home/$inv_user/mydotfiles
-
-
-echo installing my dotfiles
-cd  /home/$inv_user/mydotfiles
-
-./install.sh
 
 echo logging into github account
 gh auth login 
 
 echo cloning mypacmanpackages
-git clone https://github.com/Henry-Savary-Jackson/mypacmanpackages /home/$inv_user/mypacmanpackages
+git clone https://github.com/Henry-Savary-Jackson/mypacmanpackages ~/mypacmanpackages
+
+cd ~/mypacmanpackages
+
+./install.sh
+
+git clone https://github.com/Henry-Savary-Jackson/mydotfiles ~/mydotfiles
+
+
+echo installing my dotfiles
+cd  ~/mydotfiles
+
+./install.sh
+
 
 echo cloning mydwmbuild 
-git clone https://github.com/Henry-Savary-Jackson/mydwmbuild /home/$inv_user/mydwmbuild
+git clone https://github.com/Henry-Savary-Jackson/mydwmbuild ~/mydwmbuild
+
+cd ~/mydwmbuild
+sudo make clean install
 
 echo cloning mystbuild 
-git clone https://github.com/Henry-Savary-Jackson/mystbuild /home/$inv_user/mydwmbuild
+git clone https://github.com/Henry-Savary-Jackson/mystbuild ~/mydwmbuild
 
 echo cloning mydwmblocksbuild 
-git clone https://github.com/Henry-Savary-Jackson/mydwmblocksbuild /home/$inv_user/mydwmblocksbuild
+git clone https://github.com/Henry-Savary-Jackson/mydwmblocksbuild ~/mydwmblocksbuild
+
+cd ~/mydwmblocksbuild
+sudo make clean install
 
 echo cloning mydmenubuild 
-git clone https://github.com/Henry-Savary-Jackson/mydmenubuild /home/$inv_user/mydmenubuild
+git clone https://github.com/Henry-Savary-Jackson/mydmenubuild ~/mydmenubuild
+
+cd ~/mydmenubuild
+sudo make clean install
 
 echo cloning myrangerbuild 
-git clone https://github.com/Henry-Savary-Jackson/myrangerbuild /home/$inv_user/myrangerbuild
+git clone https://github.com/Henry-Savary-Jackson/myrangerbuild ~/myrangerbuild
+
+
+cd ~/myrangerbuild
+sudo python3 setup.py install
 
 echo cloning mynvimconfig 
-git clone https://github.com/Henry-Savary-Jackson/mynvimconfig /home/$inv_user/mynvimconfig
+git clone https://github.com/Henry-Savary-Jackson/mynvimconfig ~/mynvimconfig
 
 echo cloning password-store 
-git clone https://github.com/Henry-Savary-Jackson/mynvimhttpconfig /home/$inv_user/.password-store
+git clone https://github.com/Henry-Savary-Jackson/mynvimhttpconfig ~/.password-store
 
 echo cloning mozillaconfig 
-git clone https://github.com/Henry-Savary-Jackson/mozillaconfig /home/$inv_user/mozillaconfig
+git clone https://github.com/Henry-Savary-Jackson/mozillaconfig ~/mozillaconfig
+
+cd  ~/mozillaconfig 
+./install.sh
+
 
 echo cloning mycodingstuff 
-git clone https://github.com/Henry-Savary-Jackson/codingstuff /home/$inv_user/codingstuff
+git clone https://github.com/Henry-Savary-Jackson/codingstuff ~/codingstuff
 
 echo done
